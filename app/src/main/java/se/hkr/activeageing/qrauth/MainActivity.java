202266745b176815490f1f7904e180ecbcb0fc2a
Amer Harb <amer.harb@gmail.com>
2016-10-31 02:10:23 +0100

    check the url

--- app/src/main/java/se/hkr/activeageing/qrauth/MainActivity.java
+++ app/src/main/java/se/hkr/activeageing/qrauth/MainActivity.java
@@ -10,14 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_QR = 436;
     private TextView mTextView;
     private WebView mWebView;
+    private final String QRServer = "www.example.com"; //TODO: change this to the correct server
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.tv_main_qr);
         mWebView = (WebView) findViewById(R.id.wv_main);
+
     }
 
     @Override
@@ -72,11 +76,37 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 String contents = data.getStringExtra("SCAN_RESULT");
                 mTextView.setText(contents);
-                mWebView.loadUrl(contents);
-            }
-            if (resultCode == RESULT_CANCELED) {
-                //handle cancel
+                switch (checkURL(contents)){
+                    case Correct:
+                        mWebView.loadUrl(contents);
+                        break;
+                    case NotHTTP:
+                        Toast.makeText(getApplicationContext(), "Not HTTP protocol", Toast.LENGTH_SHORT).show();
+                        break;
+                    case WrongServer:
+                        Toast.makeText(getApplicationContext(), "Wrong Server, our server is: " + QRServer, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }else if(resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "QR Canceled", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private CheckURL checkURL(String contents) {
+
+        final String protocol = "http://";
+
+        if (!contents.toLowerCase().startsWith(protocol)) {
+            return CheckURL.NotHTTP;
+        }else if (!contents.toLowerCase().substring(protocol.length(), protocol.length() + QRServer.length()).equalsIgnoreCase(QRServer)){
+            return CheckURL.WrongServer;
+        }else{
+            return CheckURL.Correct;
+        }
+    }
+
+    private enum CheckURL{
+        Correct, NotHTTP, WrongServer
     }
 }
